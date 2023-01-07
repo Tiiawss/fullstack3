1298cf8a9d9d008ec50f1caafa53f0d38f28fb0a
Tiiawss <tiia.nylen@gmail.com>
2023-01-07 23:14:19 +0200

    4.14

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -35,13 +35,21 @@ blogsRouter.post('/', async (request, response) => {
     }
 })
 
-blogsRouter.get('/:id', async (request, response) => {
-    const blog = await Blog.findById(request.params.id)
-    if (blog) {
-        response.json(blog)
-    } else {
-        response.status(404).end()
+blogsRouter.put('/:id', async (request, response, next) => {
+    const body = request.body
+
+    const blog = {
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes || 0
     }
+    Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+        .then(updatedBlog => {
+            response.json(updatedBlog)
+        })
+        .catch(error => next(error))
+
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -11,117 +11,156 @@ beforeEach(async () => {
     await Blog.insertMany(helper.initialBlogs)
 })
 
-test('blogs are returned as json', async () => {
-    await api
-        .get('/api/blogs')
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
-}, 100000)
+describe('when there is initially some blogs saved', () => {
+    test('blogs are returned as json', async () => {
+        await api
+            .get('/api/blogs')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+    }, 100000)
 
-test('there are two blogs', async () => {
-    const response = await api.get('/api/blogs')
+    test('there are two blogs', async () => {
+        const response = await api.get('/api/blogs')
 
-    expect(response.body).toHaveLength(2)
+        expect(response.body).toHaveLength(2)
+    })
+
+    test('the first blog is about harmful', async () => {
+        const response = await api.get('/api/blogs')
+
+        expect(response.body[0].title).toBe('Go To Statement Considered Harmful')
+    })
+    test('all blogs are returned', async () => {
+        const response = await api.get('/api/blogs')
+
+        expect(response.body).toHaveLength(helper.initialBlogs.length)
+    })
+
+    test('a specific blog is within the returned blogs', async () => {
+        const response = await api.get('/api/blogs')
+
+        const contents = response.body.map(r => r.title)
+        expect(contents).toContain(
+            'Go To Statement Considered Harmful'
+        )
+    })
 })
+describe('adding blogs', () => {
+    test('a valid blog can be added', async () => {
+        const newBlog = {
+            title: '3Go To Statement Considered Harmful',
+            author: '3Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 3,
+        }
 
-test('the first blog is about harmful', async () => {
-    const response = await api.get('/api/blogs')
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+        const blogsAtEnd = await helper.blogsInDb()
+        expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
 
-    expect(response.body[0].title).toBe('Go To Statement Considered Harmful')
-})
-test('all blogs are returned', async () => {
-    const response = await api.get('/api/blogs')
+        const contents = blogsAtEnd.map(n => n.title)
+        expect(contents).toContain(
+            '3Go To Statement Considered Harmful'
+        )
+    })
+    test('the likes are 0 if not spesified', async () => {
+        const newBlog = {
+            title: '3Go To Statement Considered Harmful',
+            author: '3Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
 
-    expect(response.body).toHaveLength(helper.initialBlogs.length)
-})
+        }
 
-test('a specific blog is within the returned blogs', async () => {
-    const response = await api.get('/api/blogs')
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
 
-    const contents = response.body.map(r => r.title)
-    expect(contents).toContain(
-        'Go To Statement Considered Harmful'
-    )
-})
-test('a valid blog can be added', async () => {
-    const newBlog = {
-        title: '3Go To Statement Considered Harmful',
-        author: '3Edsger W. Dijkstra',
-        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-        likes: 3,
-    }
+        const blogsAtEnd = await helper.blogsInDb()
+        expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
 
-    await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
-    const blogsAtEnd = await helper.blogsInDb()
-    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+        const contents = blogsAtEnd.map(n => n.likes)
+        expect(contents).toContain(
+            0
+        )
+    })
+    test('blog without title is not added', async () => {
+        const newBlog = {
+            title:'',
+            author: '3Edsger W. Dijkstra',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        }
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(400)
 
-    const contents = blogsAtEnd.map(n => n.title)
-    expect(contents).toContain(
-        '3Go To Statement Considered Harmful'
-    )
-})
-test('the likes are 0 if not spesified', async () => {
-    const newBlog = {
-        title: '3Go To Statement Considered Harmful',
-        author: '3Edsger W. Dijkstra',
-        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        const blogsAtEnd = await helper.blogsInDb()
 
-    }
+        expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+    })
+    test('blog without url is not added', async () => {
+        const newBlog = {
+            title: 'jtn',
+            author: '3Edsger W. Dijkstra',
+            url:''
 
-    await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
+        }
 
-    const blogsAtEnd = await helper.blogsInDb()
-    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(400)
 
-    const contents = blogsAtEnd.map(n => n.likes)
-    expect(contents).toContain(
-        0
-    )
-})
-test('blog without title is not added', async () => {
-    const newBlog = {
-        title:'',
-        author: '3Edsger W. Dijkstra',
-        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    }
-    await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(400)
+        const blogsAtEnd = await helper.blogsInDb()
 
-    const blogsAtEnd = await helper.blogsInDb()
+        expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+    })
 
-    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
-})
-test('blog without url is not added', async () => {
-    const newBlog = {
-        title: 'jtn',
-        author: '3Edsger W. Dijkstra',
-        url:''
 
-    }
-
-    await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(400)
-
-    const blogsAtEnd = await helper.blogsInDb()
-
-    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
 })
 test('blogs have an id defined', async () => {
     const blogs = await helper.blogsInDb()
 
     expect(blogs[0].id).toBeDefined()
+})
+describe('updating a blog', () => {
+    test('updates the blog likes', async () => {
+        const blog = await Blog.findOne({ title: 'Go To Statement Considered Harmful' })
+        console.log(blog.id)
+        blog.likes = 200
+        await api
+            .put(`/api/blogs/${blog.id}`)
+            .send(blog)
+            .expect(200)
+
+        expect(blog.likes).toBe(200)
+    })
+})
+describe('deletion of a blog', () => {
+    test('succeeds with status code 204 if id is valid', async () => {
+        const blogsAtStart = await helper.blogsInDb()
+        const blogToDelete = blogsAtStart[0]
+
+        await api
+            .delete(`/api/blogs/${blogToDelete.id}`)
+            .expect(204)
+
+        const blogsAtEnd = await helper.blogsInDb()
+
+        expect(blogsAtEnd).toHaveLength(
+            helper.initialBlogs.length - 1
+        )
+
+        const contents = blogsAtEnd.map(r => r.title)
+
+        expect(contents).not.toContain(blogToDelete.title)
+    })
 })
 
 
